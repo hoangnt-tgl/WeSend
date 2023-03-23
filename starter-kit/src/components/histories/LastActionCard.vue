a02c9caa4fa4887bd477e0b09feb04bfadb4eebd
tronghoangbk <116530644+space-hoang@users.noreply.github.com>
2023-03-24 00:17:04 +0700

    history

---
+++ starter-kit/src/components/histories/LastActionCard.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {
+    }
+  },
+})
+</script>
+
+<template>
+  <VAlert
+    border="start"
+    border-color="primary"
+  >
+    Cake sweet roll sesame snaps cheesecake halvah apple pie gingerbread cake.
+  </VAlert>
+</template>
---
+++ starter-kit/src/components/histories/LastActions.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import LastActionCard from '@/components/histories/LastActionCard.vue'
+import { lastAction } from '@/data/history'
+
+export default {
+  components: {
+    LastActionCard,
+  },
+  setup() {
+    return {
+      lastAction,
+    }
+  },
+}
+</script>
+
+<template>
+  <VCard>
+    <div
+      v-for="(item, idx) in lastAction"
+      :key="idx"
+    >
+      <div class="mx-4 my-2">
+        <LastActionCard :item="item" />
+      </div>
+    </div>
+  </VCard>
+</template>

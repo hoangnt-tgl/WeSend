3a9e88540a833704a6bdb1cce5e2e577a94e3dc4
TomAbas <tructam37@gmail.com>
2023-03-24 00:04:39 +0700

    notifications card

--- starter-kit/src/views/dashboard/dashboard.vue
+++ starter-kit/src/views/dashboard/dashboard.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import CardNotifications from '@/components/dashboard/card-notifications/index.vue'
 import Cardcampaign from '@/components/dashboard/cardcampaign/index.vue'
 
 const items = [1, 2, 3, 4]
+const notifications = [{ title: 'Campaign “Halloween Discount” was deleted.', date: '24 Sep 2022 / 04:30 AM', type: 'deleted' }, { title: 'Campaign “Halloween Discount” was deleted.', date: '24 Sep 2022 / 04:30 AM', type: 'create' }, { title: 'Campaign “Halloween Discount” was deleted.', date: '24 Sep 2022 / 04:30 AM', type: 'overrided' }, { title: 'Campaign “Halloween Discount” was deleted.', date: '24 Sep 2022 / 04:30 AM', type: 'overrided' }]
 </script>
 
 <template>
@@ -20,14 +22,27 @@ const items = [1, 2, 3, 4]
     <Cardcampaign />
     <Cardcampaign />
   </div>
+  <div class="notifications-wrapper">
+    <div
+      v-for="notification in notifications"
+      :key="notification.title"
+      :notification="notification"
+    >
+      <CardNotifications
+        :type="notification.type"
+        :date="notification.date"
+        :title="notification.title"
+      />
+    </div>
+  </div>
 </template>
 
 <style>
-@media (min-width: 375px) {
+/* @media (min-width: 375px) {
   .campaign-wrapper {
 
   }
-}
+} */
 
 @media (min-width: 768px) {
   .campaign-wrapper {

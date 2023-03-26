863358e84bb8970339db3c61e4d9c9b4ef494279
tronghoangbk <116530644+space-hoang@users.noreply.github.com>
2023-03-26 21:50:32 +0700

    build

---
+++ starter-kit/dist/assets/VSpacer.a3f65175.js
@@ -0,0 +1,1 @@
+import{C as e}from"./index.02e136f0.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
--- starter-kit/dist/assets/index.222822ef.js
+++
@@ -1,1 +0,0 @@
-import{b2 as e,o as c,c as o}from"./index.91ca553b.js";const n={};function r(t,l){return c(),o("div",null," Hello World ")}const a=e(n,[["render",r]]);export{a as default};

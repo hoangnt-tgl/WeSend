005441ff85ddb9a6245d76afd087587cd17646b8
tronghoangbk <116530644+space-hoang@users.noreply.github.com>
2023-03-28 23:11:44 +0700

    m

---
+++ starter-kit/src/components/modal/CampaignSchedule.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+const isDialogVisible = ref(true)
+</script>
+
+<template>
+  <VDialog
+    v-model="isDialogVisible"
+    persistent
+    class="v-dialog-sm"
+  >
+    <!-- Dialog close btn -->
+    <DialogCloseBtn @click="isDialogVisible = !isDialogVisible" />
+
+    <!-- Dialog Content -->
+    <VCard title="Use Google's location service?">
+      <VCardText>
+        Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+      </VCardText>
+
+      <VCardText class="d-flex justify-end gap-3 flex-wrap">
+        <VBtn
+          color="secondary"
+          variant="tonal"
+          @click="isDialogVisible = false"
+        >
+          Disagree
+        </VBtn>
+        <VBtn @click="isDialogVisible = false">
+          Agree
+        </VBtn>
+      </VCardText>
+    </VCard>
+  </VDialog>
+</template>

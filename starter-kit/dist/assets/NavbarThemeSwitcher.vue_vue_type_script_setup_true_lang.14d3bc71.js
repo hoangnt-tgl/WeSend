a2c9be563e20684fb75da72811953d5c20d46748
tronghoangbk <116530644+space-hoang@users.noreply.github.com>
2023-03-31 00:40:54 +0700

    fix ms2

---
+++ starter-kit/dist/assets/NavbarThemeSwitcher.vue_vue_type_script_setup_true_lang.14d3bc71.js
@@ -0,0 +1,1 @@
+import{d as e,o,c as r}from"./index.d6ea0e9d.js";const _=e({__name:"NavbarThemeSwitcher",setup(t){return(a,c)=>(o(),r("h1"))}});export{_};
---
+++ starter-kit/dist/assets/VSpacer.eba4298e.js
@@ -0,0 +1,1 @@
+import{D as e}from"./index.d6ea0e9d.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};

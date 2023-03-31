30e5d2e4aa7ee2681f179f0afbf00893b67ea8d5
TomAbas <tructam37@gmail.com>
2023-03-31 15:54:07 +0700

    123

---
+++ starter-kit/src/components/datepicker/DatePicker.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = null
+export default {
+  name: 'Datepicker',
+  components: {
+    VueDatePicker,
+  },
+  setup() {
+    return {
+      date,
+    }
+  },
+}
+</script>
+
+<template>
+  <VueDatePicker
+    v-model="date"
+    calendar-cell-class-name="dp-custom-cell"
+    :offset="30"
+  >
+    <template #input-icon>
+      <VIcon icon="tabler-calendar-due" />
+    </template>
+  </VueDatePicker>
+</template>
+
+<style>
+.dp__main {
+  /* flex: 1; */
+
+  /* inline-size: fit-content; */
+  inline-size: 15%;
+}
+
+.dp-custom-cell {
+  border-radius: 50%;
+}
+
+.dp__input {
+  display: none;
+}
+</style>

5aeb68f0e3b88d7938d0fd8e45a8677f3aa94fb7
tronghoangbk <116530644+space-hoang@users.noreply.github.com>
2023-04-01 13:13:36 +0700

    ms3 start

--- starter-kit/src/pages/calendar/index.vue
+++ starter-kit/src/pages/calendar/index.vue
@@ -1,5 +1,31 @@
+<script>
+import { defineComponent } from 'vue'
+import Meeting1 from '@/components/modal/Meeting1.vue'
+import Meeting2 from '@/components/modal/Meeting2.vue'
+import Meeting3 from '@/components/modal/Meeting3.vue'
+
+export default defineComponent({
+  name: 'Calendar',
+  components: {
+    Meeting1,
+    Meeting2,
+    Meeting3,
+  },
+  setup() {
+    return {
+      openMeeting1: ref(false),
+      openMeeting2: ref(false),
+      openMeeting3: ref(false),
+    }
+  },
+})
+</script>
+
 <template>
   <div>
+    <Meeting1 v-if="openMeeting1" />
+    <Meeting2 v-if="openMeeting1" />
+    <Meeting3 v-if="openMeeting1" />
     Hello World
   </div>
 </template>
